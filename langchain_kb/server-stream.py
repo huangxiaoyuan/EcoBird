e2001417f68fe1279b396852b4fc089fbf32674d
huangxiaoyuan <1260851762@qq.com>
2026-04-25 15:09:08 +0800

    Update server-stream.py

--- langchain_kb/server-stream.py
+++ langchain_kb/server-stream.py
@@ -23,7 +23,7 @@ os.environ[
     "USER_AGENT"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 # ================= 配置 =================
-API_KEY = "sk-nzxutftishvwoayxihtceizdtdbwmomewcoofwhyritpcjaz"
+API_KEY = "XXXXXX"
 BASE_URL = "https://api.siliconflow.cn/v1"
 DB_PATH = "./faiss_db"  # 修改路径名
 
